5faef8c0fcf8e9f13c18170dd01973c9cc9d673f
Jahanara Khatun <zarakhatun58@gmail.com>
2025-11-20 21:10:19 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import * as path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
